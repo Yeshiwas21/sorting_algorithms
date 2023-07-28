55c7c9cee95a7c9756d6cf4fbd2e1c5a5b2f3bb8
yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-07-28 14:09:46 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,24 +7,24 @@
  */
 void bubble_sort(int *arr, size_t size)
 {
-    unsigned int i, j;
-    int tmp;
+unsigned int i, j;
+int tmp;
 
-    if (size < 2 || arr == NULL)
-        return;
+if (size < 2 || arr == NULL)
+    return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                /* Swap the elements */
-                tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-                print_array(arr, size);
-            }
-        }
-    }
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (arr[j] > arr[j + 1])
+{
+/* Swap the elements */
+tmp = arr[j];
+arr[j] = arr[j + 1];
+arr[j + 1] = tmp;
+print_array(arr, size);
+}
+}
+}
 }
